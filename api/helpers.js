31754d4cf10f1a38052902f302b216527916c975
Jake Posner <jposner91@gmail.com>
2020-03-08 16:52:21 -0400

    refactored code a little, added onclick to log the gif url to the console. plan to give a way for people to share them easily

---
+++ api/helpers.js
@@ -0,0 +1,6 @@
+/**
+ * helper function to print to the console (kinda pointless)
+ */
+function printToConsole(value) {
+    console.log(value)
+}
--- api/script.js
+++ api/script.js
@@ -1,6 +1,9 @@
 var request = new XMLHttpRequest()
 
 var randomGiphyApiUrl = 'https://api.giphy.com/v1/gifs/random?'
+var searchGiphyApiUrl = 'api.giphy.com/v1/gifs/search'
+var gifUrlMap = {}
+
 var tag = "&tag={tagValue}"
 var rating = "&rating={ratingValue}"
 
@@ -8,9 +11,10 @@ tag = tag.replace("{tagValue}", "")
 rating = rating.replace("{ratingValue}", "g")
 request.open('GET', randomGiphyApiUrl + 'api_key=dFSL26MjqpUEiP2CxvJnUxO3tOfXi51e' + tag + rating)
 
-// var button = document.createElement('BUTTON')
-// button.innerHTML = "Randomize"
-// button.onclick = () => {
+/**
+ * function that gets a random gif from giphy using the
+ * input provided or a blank string
+ */
 function randomize() {
     var input = document.getElementById('input').value
     var tag = "&tag={tagValue}"
@@ -25,21 +29,38 @@ function randomize() {
         var gifData = parsedData.data
         var gifUrl = gifData.url
         var originalGifUrl = gifData.images.original.url
+        addGifToContainer(originalGifUrl)
+    }
+}
 
-        // Add a vertical stream of gifs
-        var gif = document.createElement('img')
-        gif.setAttribute('src', originalGifUrl)
-        gif.setAttribute('id', "gif:" + originalGifUrl)
-        gifContainer = document.getElementById("gifContainer")
-        gifContainer.insertBefore(gif, gifContainer.childNodes[0]);
+/**
+ * function that adds a gif img to the gif container with the supplied url
+ * @param {string} gifUrl
+ */
+function addGifToContainer(gifUrl) {
+     var gif = document.createElement('img')
+     gif.setAttribute('src', gifUrl)
+     gif.setAttribute('id', "gif:" + gifUrl)
+     gif.setAttribute('onclick', 'printToConsole(this.id)')
+     gifContainer = document.getElementById("gifContainer")
+     gifContainer.insertBefore(gif, gifContainer.childNodes[0]);
+}
 
-        // Debugging
-        // console.log("ParsedData:")
-        // console.log(parsedData.data)
-        // console.log('Gif URL:')
-        // console.log(gifUrl)
-        // console.log('Original URL:')
-        // console.log(originalGifUrl)
+function search() {
+    var input = document.getElementById('input').value
+    var q = "&q={qValue}"
+    q = q.replace("{qValue}", input)
+
+    rating = rating.replace("{ratingValue}", "")
+    request.open('GET', searchGiphyApiUrl + 'api_key=dFSL26MjqpUEiP2CxvJnUxO3tOfXi51e' + q + rating)
+    request.send()
+    request.onload = function() {
+        var response = request.response
+        var parsedData = JSON.parse(response)
+        var gifData = parsedData.data
+        var gifUrl = gifData.url
+        var originalGifUrl = gifData.images.original.url
+        addGifToContainer(originalGifUrl)
     }
 }
 
@@ -49,20 +70,7 @@ request.onload = function() {
     var gifData = parsedData.data
     var gifUrl = gifData.url
     var originalGifUrl = gifData.images.original.url
-
-    var gif = document.createElement('img')
-    gif.setAttribute('src', originalGifUrl)
-    gif.setAttribute('id', "gif:" + originalGifUrl)
-
-    document.getElementById("gifContainer").appendChild(gif)
-
-    // Debugging
-    console.log("ParsedData:")
-    console.log(parsedData.data)
-    console.log('Gif URL:')
-    console.log(gifUrl)
-    console.log('Original URL:')
-    console.log(originalGifUrl)
+    addGifToContainer(originalGifUrl)
 }
 
 request.send()
